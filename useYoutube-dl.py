c1f107812928afe57dc4743057d98f44ffc7253a
ofargally <ofargally@colgate.edu>
2025-02-25 10:55:03 -0500

    set up

---
+++ useYoutube-dl.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+import youtube_dl
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+link = os.getenv('YOUTUBE_LINK')
+
+print("Youtube Link: ", link)
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(
+        [link])
