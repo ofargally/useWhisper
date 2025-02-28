d187535c056af37fbd36bc40aa0b6162fc2c893e
ofargally <ofargally@colgate.edu>
2025-02-27 21:08:27 -0500

    fixed a little

--- useCrew-ai.py
+++ useCrew-ai.py
@@ -2,5 +2,13 @@ import os
 import warnings
 from dotenv import load_dotenv
 from crewai import Agent, Task, Crew
+from openai import OpenAI
+
 load_dotenv()
 warnings.filterwarnings('ignore')
+
+model = os.getenv("MODEL")
+api_key = os.getenv("API_KEY")
+base_url = os.getenv("BASE_URL")
+# Initialize the api client for chat completions
+client = OpenAI(api_key=api_key, base_url=base_url)
